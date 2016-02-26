46d420aca460e7cbbace4801bb0f31cf38acf99d
raschlemper <raschlemper@gmail.com>
2016-02-26 10:13:56 -0300

    Delete RowMapper.java

--- src/java/com/portalpostal/dao/RowMapper.java
+++
@@ -1,8 +0,0 @@
-package com.portalpostal.dao;
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-public interface RowMapper<T> {
-    public T mapRow(ResultSet rs, int rowNum) throws SQLException;
-}
